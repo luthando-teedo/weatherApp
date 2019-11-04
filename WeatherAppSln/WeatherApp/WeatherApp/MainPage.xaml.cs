261b4d24046d82ecdbc7b4dd74962a6cd9fcb620
luthando Sitole <luthando@quirky30.co.za>
2019-11-04 16:07:39 +0200

    My weatherApp

---
+++ WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DisplayStuff();
+        }
+
+        
+        public async void DisplayStuff()
+        {
+            ActivityInd.IsVisible = true;
+            ActivityInd.IsRunning = true;
+            var stuff = await WeatherReport.GetWeatherReportAsync();
+            largeNumber.Text = stuff;
+
+            var windSpeed = await WeatherReport.GetWindAsync();
+            Windspeed.Text = windSpeed;
+            ActivityInd.IsVisible = false;
+            ActivityInd.IsRunning = false;
+        }
+
+        
+        
+    }
+}
---
+++ WeatherAppSln/WeatherApp/WeatherApp/WeatherReport.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class WeatherReport
+    {
+        public static async Task<string> GetWeatherReportAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string weatherReporter = await httpClient.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20town&APPID=475eaa798f0db7fdcc9aa7f7629ef9d5&units=metric");
+
+            CapeTownWeather weather = JsonConvert.DeserializeObject<CapeTownWeather>(weatherReporter);
+            int humidity = weather.main.humidity;
+            int Temperature = Convert.ToInt32 (weather.main.temp);
+            string weatherTellTemp = $"{Temperature}";
+
+            string outPut = $"{weatherTellTemp} ";
+
+            return outPut;
+
+        }
+
+
+        public static async Task<string> GetWindAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string weatherReporter = await httpClient.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20town&APPID=475eaa798f0db7fdcc9aa7f7629ef9d5&units=metric");
+
+            CapeTownWeather weather = JsonConvert.DeserializeObject<CapeTownWeather>(weatherReporter);
+            double Wind = weather.wind.speed;
+            string WindSpeed = $"{Wind}";
+
+            string outPut = $"{WindSpeed} ";
+
+            return outPut;
+
+        }
+    }
+}
